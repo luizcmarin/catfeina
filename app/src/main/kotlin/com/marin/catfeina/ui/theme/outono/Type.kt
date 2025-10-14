0e13f545694648c19ba686e4d2a83fc510ab6e5c
Luiz Marin <67489841+luizcmarin@users.noreply.github.com>
2025-10-13 22:58:21 -0300

    Update files

--- app/src/main/kotlin/com/marin/catfeina/ui/theme/outono/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.marin.catfeina.ui.theme.outono
-
-import androidx.compose.material3.Typography
-
-val AppTypography = Typography()
--- app/src/main/kotlin/com/marin/catfeina/ui/theme/primavera/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.marin.catfeina.ui.theme.primavera
-
-import androidx.compose.material3.Typography
-
-val AppTypography = Typography()
